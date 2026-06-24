cf45f9ea364de884200112c618961d76bedcae9b
Suraj Kumar <surajkumar44727@gmail.com>
2026-06-24 16:38:14 +0530

    add vercel url

--- FRONTEND/src/utils/axios.js
+++ FRONTEND/src/utils/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({
-    baseURL: 'https://fridgemartsql.onrender.com' || '/api',
-    withCredentials: true,
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL + "/api",
 });
 
 api.interceptors.response.use(
